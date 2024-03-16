056f67876d4044251997ad670d907094151f06dc
ovifernandez <128384268+ovifernandez@users.noreply.github.com>
2024-03-16 11:51:29 +0100

    Contadores y Reinicio Funcional

--- Banabet/ViewModel/MainViewModel.cs
+++ Banabet/ViewModel/MainViewModel.cs
@@ -16,7 +16,7 @@ public partial class MainViewModel : ObservableObject
     float precioKgBanana = 1;
     //Del forms
     [ObservableProperty]
-    float apuestaMensual = 500;
+    float apuestaMensual = 500000;
     [ObservableProperty]
     float contador;
     [ObservableProperty]
