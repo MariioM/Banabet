9b081de178968cdfdddc7ba7cc61588df4802290
Mario Martínez Vitutia <9205625@alumnos.ufv.es>
2024-04-17 17:57:21 +0200

    Funciona Reset

--- Banabet/ProfilePage.xaml.cs
+++ Banabet/ProfilePage.xaml.cs
@@ -1,14 +1,17 @@
 using Banabet.Services;
+using Banabet.ViewModel;
 
 namespace BanaBet;
 
 public partial class ProfilePage : ContentPage
 {
 	private readonly AuthService _authService;
-	public ProfilePage(AuthService authService)
+	private readonly MainViewModel _mainViewModel;
+	public ProfilePage(AuthService authService, MainViewModel mainViewModel)
 	{
 		InitializeComponent();
 		_authService = authService;
+		_mainViewModel = mainViewModel;
 	}
 
 	private async void TapLogOut(object sender, EventArgs e)
@@ -16,4 +19,13 @@ public partial class ProfilePage : ContentPage
 		_authService.Logout();
 		await Shell.Current.GoToAsync(nameof(RegisterPage));
 	}
+
+	private void TapReset(object sender, EventArgs args)
+	{
+		_mainViewModel.ReinicioPublico();
+	}
+    private async void TapSet(object sender, EventArgs args)
+    {
+        await _mainViewModel.EmpezarPublico();
+    }
 }
